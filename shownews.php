ccd8fee799fbf6879d0278e396099d9f6fa504de
Denny Hadikusumo <denny0692@yahoo.com>
2014-05-03 13:14:36 +0700

    UPDATE NEWS
    MERUBAH BERITA...

--- shownews.php
+++ shownews.php
@@ -1,7 +1,7 @@
 <?php include "connection.php"; ?>
 <html>
 <head>
-<title>Lembaga Pengembangan PESPARAWI Nasional (LPPN)</title>
+<title>TEKNIK INFORMATIKA UNIVERSITAS SILIWANGI</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <link href="style.css" rel="stylesheet" type="text/css">
 </head>
@@ -22,21 +22,25 @@
           <td height="39" valign="top" background="images/bg_header.gif"><h3>NEWS</h3></td>
         </tr>
         <tr>
-          <td><span class="headline">KISAH SEJATI SEORANG KAKA</span><br>
-			Lahir di Brasilia tahun 1982 dengan nama Ricardo Izecson dos Santos Leite, Kaka lahir dari sebuah keluarga penginjil yang kaya raya. Namun hal itu tidak membuat ia menjadi sombong dengan mengandalkan kekayaan keluarganya, ataupun mengikuti jalan hidup keluarganya dengan menjadi penginjil. Kaka punya jalannya sendiri dan caranya sendiri.<br>
+          <td><span class="headline">Perbedaan Jurusan Ilmu Komputer, Teknik Informatika, Sistem Informasi dan Teknik Elektro</span><br>
+			Pertama, Jurusan Ilmu Komputer itu adalah jurusan yang mempelajari segala hal yang berkaitan dengan komputer, baik dari segi Hardware maupun softwarenya.Bagi yang berminat di bidang Hardware lebih baik mengambil jurusan teknik komputer, sedangkan bagi yang lebih berminat pada bidang software dapat memperdalam di jurusan Teknik Informatika..<br>
 			<br>
-			Sejak kecil ia sangat menyukai sepakbola, bahkan dalam usia remaja ia menjadi pemain yang cukup terkenal didaerahnya dengan bermain sebagai pemain cadangan di klub San Paulo.<br>			<br>
-			Namun pada usia 18 tahun sebuah bencana terjadi, ia mengalami cidera punggung yang serius saat sedang berenang. Dokter mengatakan ia tidak bisa bermain sepakbola lagi, bahkan kemungkinan besar akan lumpuh akbibat cidera itu. Tidak ada tindakan operasi atau terapi yang bisa menyelamatkannya.<br>			<br>
-			Hidup Kaka hancur berantakan saat itu, kecintaannya pada sepakbola demikian besar, kini semua harus berakhir, bahkan sisa hidupnya harus diisi dengan menjalani kelumpuhannya.<br>
+			dan menurut Wikipedia, Ilmu komputer (bahasa Inggris: Computer Science), secara umum diartikan sebagai ilmu yang mempelajari baik tentang komputasi, perangkat keras (hardware) maupun perangkat lunak (software). Ilmu komputer mencakup beragam topik yang berkaitan dengan komputer, mulai dari analisa abstrak algoritma sampai subyek yang lebih konkret seperti bahasa pemrograman, perangkat lunak, termasuk perangkat keras. Sebagai suatu disiplin ilmu, Ilmu Komputer lebih menekankan pada pemrograman komputer, dan rekayasa perangkat lunak (software), sementara teknik komputer lebih cenderung berkaitan dengan hal-hal seperti perangkat keras komputer (hardware). Namun demikian, kedua istilah tersebut sering disalah-artikan oleh banyak orang.<br>			<br>
+			Prospek / Lapangan kerja bagi lulusan Ilmu Komputer:
+			Sarjana Ilmu Komputer sering dibutuhkan hampir di semua perusahaan karena pada saat ini tidak terlepas dari peranan komputer.<br>			<br>
+			Teknik Informatika adalah jurusan yang mempelajari bagaimana logika-logika matematika yang digunakan pada sistem informasi. Perancangan desain informasi, pembuatan software, web design, dll merupakan hal yang menjadi bidang garap jurusan ini Kemampuan di bidang matematika akan sangat diperlukan dalam mendalami bidang informatika.<br>
 			Namun Kaka tahu kemana ia harus minta tolong saat dokter sudah angkat tangan. Kaka bergumul dengan Tuhan, tak putus-putusnya ia berdoa memohon kesembuhannya. Ia bernazar pada Tuhan, bila ia sembuh dan dapat bermain sepakbola lagi, ia akan mempersembahkan seluruh prestasinya itu pada Tuhan Yesus.<br>			<br>
-			Dan keajaibanpun terjadi, setahun setelah kecelakaannya itu tepatnya tahun 2001, Tuhan menyembuhkannya, ia sembuh total dari sakitnya. Bahkan ia dapat merumput bermain sepakbola lagi. Tuhan juga memberikan hadiah bonus, ia tidak lagi menjadi pemain cadangan melainkan menjadi pemain utama dan andalan dalam klubnya.<br>			<br>
+			Prospek / Lapangan kerja bagi lulusan Teknik Informatika:
+			Berbagai perusahaan yang berkaitan dengan informasi dan komunikasi sangat membutuhkan jurusan ini, seperti PT Telkom, Indosat dan dapat bekerja mandiri sebagai programming, perancang web dll.<br>			<br>
 			Tuhan membuat permainan Kaka menjadi begitu hebat sehingga manager tim nasional Brazil terpikat akan permainannya, dan memanggil Kaka untuk mengenakan baju kebesaran tim Brazil, emas dan hijau, dipercaya untuk bertarung di piala dunia 2002.<br>			<br>
-			Dari sekian banyak bakat baru bersinar di Brazil, ia hanyalah seorang pemain muda yang belum setahun membela klubnya, namun sudah dipanggil masuk tim nasional. Bagi Kaka itu adalah keajaiban dan anugerah yang besar baginya.<br>
-			Walaupun dia hanya jadi pemain cadangan dan duduk dipinggir lapangan menonton pertandingan para seniornya di piala dunia, namun Kaka sudah sangat senang dapat ikut serta dalam kompetisi sebesar piala dunia. Kaka tidak menyadari Tuhan sedang menyediakan keajaiban lainnya bagi dia.<br>			<br>
-			Beberapa pertandingan berjalan begitu keras bagi Brazil, sehingga beberapa pemain bintang harus disimpan karena cidera. Datanglah kesempatan bagi Kaka untuk turun membela timnya. Dibawah pembelaannya Brazilpun menang, peristiwa legendaris yang menggemparkan dunia itupun terjadi, Kaka mengangkat seragam-nya dan di baliknya ada sebuah tulisan yang menggegerkan, kaos putih itu bertuliskan &quot;I Love Jesus&quot;.<br>			<br>
-			Itu terus dilakukannya setiap kali teman-temannya merayakan gol. Dan akhirnya Brazil-pun memenangkan Piala Dunia 2002, setelah menaklukan Jerman di final dengan skor 2-0. Dalam parade kemenangan dinegaranya sendiri, kaos kesayangan yang bertuliskan 'I love Jesus' itu tidak pernah dilepasnya. Hal itu menginspirasi banyak pemain Brazil (bahkan pemain negara lain) melakukan hal yang sama.<br>			<br>
+			Sistem Informasi adalah jurusan yang pada dasarnya masih berkaitan dengan informatika. Bisa dikatakan bahwa sistem informasi merupakan salah satu aplikasi dari Teknik Informatika.<br>
+			menurut Wikipedia, Sistem Informasi adalah aplikasi komputer untuk mendukung operasi dari suatu organisasi yaitu: operasi, instalasi, dan perawatan komputer, perangkat lunak, dan data. Sistem Informasi Manajemen adalah kunci dari bidang yang menekankan finansial dan personal manajemen. 'Sistem Informasi' dapat berupa gabungan dari beberapa elemen teknologi berbasis komputer yang saling berinteraksi dan bekerja sama berdasarkan suatu prosedur kerja (aturan kerja) yang telah ditetapkan, dimana memproses dan mengolah data menjadi suatu bentuk informasi yang dapat digunakan dalam mendukung keputusan.<br>			<br>
+			Prospek / Lapangan kerja bagi lulusan Sistem Informasi:
+			Berbagai perusahaan seluler, Stasiun Televisi, Departemen Perhubungan, Departemen Tenaga Kerja dan kantor-kantor lain yang banyak menggunakan database sebagai acuan &quot;I Love IT&quot;.<br>			<br>
+			Teknik Elektro adalah jurusan yang berkaitan dengan kelistrikan. Karena keluasan bidang kelistrikan maka teknik elektro mempunyai berbagaispesialisasi antara lain, Teknik ketenagaan (arus kuat) Telekomunikasi (arus lemah), Sistem kontrol, Elektronika dan Komputer..<br>			<br>
 			Saat diwawancara oleh stasiun TV dan ditanya mengapa ia melakukan hal itu, ia berkata, &quot;Saya ingin memperlihatkan dengan hidup dan kerja saya, apa yang telah Tuhan lakukan bagi saya, supaya orang lain dapat melihat apa yang Tuhan bisa lakukan dalam kehidupan mereka.&quot;<br>			<br>
-			Permainannya yang cantik di Piala Dunia tidak luput dari perhatian sebuah klub raksasa di Italia, AC Milan. Tidak lama kemudian mereka meminta Kaka masuk dalam timnya sebagai pemain utama. Kaka-pun pindah bergabung dengan AC Milan, masuk dalam liga Italia yang keras dan penuh bintang. Namun dalam musim pertamanya di Liga Italia seri A, ia langsung menyumbangkan gelar juara scudetto bagi AC Milan.<br>			<br>
+			Prospek / Lapangan kerja bagi lulusan Teknik Elektro:
+			Berbagai perusahaan seperti PLN, Telkom, Indosat dan menjadi dosen di perguruan tinggi.<br>			<br>
 			Dalam waktu singkat Kaka menjadi bintang dan pujaan banyak orang khususnya wanita, kegantengannya yang seperti seorang bintang film membuat ia selalu dikejar-kejar fans wanita, dimanapun ia berada akan selalu ada jeritan gadis-gadis muda yang mengaguminya.<br>			<br>
 			Namun cinta dan kesetiannya hanya pada Caroline Celico, kekasihnya yang jauh di Brazil. Walaupun kehidupan pemain sepakbola selalu dikeliling wanita-wanita cantik super model, atau pesta-pesta kemenangan, Kaka selalu menghindari semuanya itu. Ia bahkan tidak mau membawa Caroline tinggal dengannya di Italia sebelum pernikahan, seperti yang dilakukan para pemain bola di liga-liga besar.<br>
 			<br>
