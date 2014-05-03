830188b6cbd410d96e2671f9c4af859efea00b19
galihfadllulloh <galihfadllulloh@yahoo.co.id>
2014-05-03 13:41:19 +0700

    UPDATE KETUA

--- ketua.php
+++ ketua.php
@@ -5,19 +5,19 @@
 	<tr>
 		<td valign="top"><div align="justify"><table width="100%"  border="0" cellspacing="0" cellpadding="0" class="mini-font">
             <tr valign="top">
-              <td colspan="3">&#149;&nbsp; KETUA UMUM</td>
+              <td colspan="3">&#149;&nbsp;DEKAN </td>
             </tr>
             <tr valign="top">
               <td width="13%">NAMA</td>
               <td width="5%" align="center">:</td>
-              <td width="82%">Dr. Jason Lase, S.Th, M.Si</td>
+              <td width="82%">Nina Herlina</td>
             </tr>
             <tr valign="top">
               <td>ALAMAT</td>
               <td align="center">:</td>
-              <td>Jl. Pengangsaan Indah Barat 2 B <br>
-              Pondok Gading Utama Blok 0/32 Pengangsaan Dua <br>
-              Jakarta 14250</td>
+              <td>Jl. SILIWANGI <br>
+              
+              TASIKMALAYA</td>
             </tr>
             <tr valign="top">
               <td>&nbsp;</td>
@@ -25,76 +25,20 @@
               <td>&nbsp;</td>
             </tr>
             <tr valign="top">
-              <td colspan="3">&#149;&nbsp; KETUA PELAKSANA HARIAN</td>
+              <td colspan="3">&#149;&nbsp; KETUA JURUSAN</td>
             </tr>
             <tr valign="top">
               <td>NAMA</td>
               <td align="center">:</td>
-              <td>Drs. Mangaratua Bakara</td>
+              <td>Aradea ST.MT</td>
             </tr>
             <tr valign="top">
               <td>ALAMAT</td>
               <td align="center">:</td>
-              <td>Komp. Dep. Keuangan No. C.67 <br>
-              Jakarta 11610</td>
+              <td>Jl. SILIWANGI <br>
+              TASIKMALAYA</td>
             </tr>
-            <tr valign="top">
-              <td>&nbsp;</td>
-              <td align="center">&nbsp;</td>
-              <td>&nbsp;</td>
-            </tr>
-            <tr valign="top">
-              <td colspan="3">&#149;&nbsp; KETUA I</td>
-            </tr>
-            <tr valign="top">
-              <td>NAMA</td>
-              <td align="center">:</td>
-              <td>Aida Swenson Simanjuntak</td>
-            </tr>
-            <tr valign="top">
-              <td>ALAMAT</td>
-              <td align="center">:</td>
-              <td>Bintaro Paradise Kav. 1 <br>
-              Jakarta 12330</td>
-            </tr>
-            <tr valign="top">
-              <td>&nbsp;</td>
-              <td align="center">&nbsp;</td>
-              <td>&nbsp;</td>
-            </tr>
-            <tr valign="top">
-              <td colspan="3">&#149;&nbsp; KETUA II</td>
-            </tr>
-            <tr valign="top">
-              <td>NAMA</td>
-              <td align="center">:</td>
-              <td>John RP. Hutabarat, SE, MA</td>
-            </tr>
-            <tr valign="top">
-              <td>ALAMAT</td>
-              <td align="center">:</td>
-              <td>Komp. Chandra Indah <br>
-              Jl. Irian Jaya Blok E No.55, Pondok Gede <br>
-              Bekasi 17414</td>
-            </tr>
-            <tr valign="top">
-              <td>&nbsp;</td>
-              <td align="center">&nbsp;</td>
-              <td>&nbsp;</td>
-            </tr>
-            <tr valign="top">
-              <td colspan="3">&#149;&nbsp; KETUA III</td>
-            </tr>
-            <tr valign="top">
-              <td>NAMA</td>
-              <td align="center">:</td>
-              <td>Pdt. Weinata Sairin, M.Th</td>
-            </tr>
-            <tr valign="top">
-              <td>ALAMAT</td>
-              <td align="center">:</td>
-              <td>Taman Bougenville Estate Blok E-II No.6 Kalimalang, <br>
-              Bekasi 17412</td>
+            </td>
             </tr>
             <tr valign="top">
               <td>&nbsp;</td>
