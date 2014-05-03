17ee1865b8afc7dac9d85599c46900d47e716699
galihfadllulloh <galihfadllulloh@yahoo.co.id>
2014-05-03 14:02:29 +0700

    UPDATE ABOUT

--- aboutus.php
+++ aboutus.php
@@ -1,7 +1,7 @@
 <?php include "connection.php"; ?>
 <html>
 <head>
-<title>Lembaga Pengembangan PESPARAWI Nasional (LPPN)</title>
+<title>JURUSAN TEKNIK INFORMATIA</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <link href="style.css" rel="stylesheet" type="text/css">
 </head>
@@ -23,79 +23,9 @@
         </tr>
         <tr>
           <td><div align="justify">
-            LPPN adalah singkatan dari Lembaga Pengembangan Pesta Paduan Suara Gerejawi (PESPARAWI) Nasional.
-                <br>
-              <br>
-            <table width="100%"  border="0" cellpadding="0" cellspacing="0" class="mini-font">
-              <tr>
-                <td colspan="2" align="left" valign="top"> <div align="justify">Tugas LPPN berdasarkan <em>PMA No. 19 Tahun 2005 Pasal 3 dan Hasil Konsultasi Ditjen Bimas Kristen, LPPN dan LPPD Se-Indonesia </em>adalah :</div></td>
-                </tr><tr>
-                <td width="5%" align="left" valign="top">1.</td>
-                <td width="95%" valign="top"> <div align="justify">Lembaga Pengembangan PESPARAWI Nasional (LPPN) mempunyai tugas membina Lembaga Pengembangan Pesta Paduan Suara Gerejawi (PESPARAWI) Daerah (LPPD) dan meningkatkan kualitas Paduan Suara Gerejawi dalam kerjasama dengan gereja-gereja di Indonesia.</div></td>
-              </tr>
-              <tr>
-                <td align="left" valign="top">2.</td>
-                <td valign="top"><div align="justify"> Membentuk asosiasi/forum/wadah musisi Kristen, Museum Seni dan Budaya Kristiani dan paduan suara percontohan untuk pengembangan/peningkatan kualitas paduan suara, musik serta seni budaya gerejawi.</div></td>
-              </tr>
-              <tr>
-                <td align="left" valign="top">3.</td>
-                <td valign="top"><div align="justify"> Mengadakan Kerjasama dengan institusi/organisasi seni dan budaya luar negeri dalam rangka pengembangan/peningkatan kualitas PESPARAWI dan penyelenggaraan PESPARAWI ASEAN.</div></td>
-              </tr>
-            </table><br>
-			<table width="100%"  border="0" cellpadding="0" cellspacing="0" class="mini-font">
-              <tr>
-                <td colspan="2" align="left" valign="top"> <div align="justify"> Fungsi LPPN berdasarkan <em>PMA No. 19 Tahun 2005 Pasal 4 </em> adalah <em></em>:</div></td>
-                </tr><tr>
-                <td width="5%" align="left" valign="top">a.</td>
-                <td width="95%" valign="top"> <div align="justify"> Perumusan Visi, Misi dan ketentuan pelaksanaan pengembangan serta peningkatan kualitas musik gerejawi.</div></td>
-              </tr>
-              <tr>
-                <td align="left" valign="top">b.</td>
-                <td valign="top"><div align="justify"> Pembinaan, pelayanan dan bimbingan kepada LPPD di bidang musik gerejawi, lomba cipta lagu gerejawi, kursus/penataran, pembinaan musisi Kristen, dirigen dan paduan suara gereja.</div></td>
-              </tr>
-              <tr>
-                <td align="left" valign="top">c.</td>
-                <td valign="top"><div align="justify"> Penetapan musik dan lagu-lagu gerejawi sebagai sarana untuk memuji Tuhan dan memupuk rasa persaudaraan sebagai ungkapan kesetiaan kepada Tuhan Yang Maha Esa.</div></td>
-              </tr>
-              <tr>
-                <td align="left" valign="top">d.</td>
-                <td valign="top"><div align="justify"> Pengkoordinasian, perencanaan, pelaksanaan, pengendalian dan pengawasan program.</div></td>
-              </tr>
-              <tr>
-                <td align="left" valign="top">e.</td>
-                <td valign="top"><div align="justify"> Pelaksanaan hubungan dengan pemerintah pusat, pemerintah daerah, lembaga gereja atas nasional dan instansi lainnya.</div></td>
-              </tr>
-              <tr>
-                <td align="left" valign="top">f.</td>
-                <td valign="top"><div align="justify"> Penyelenggaraan administrasi dan informasi.</div></td>
-              </tr>
-            </table><br>
-			<table width="100%"  border="0" cellpadding="0" cellspacing="0" class="mini-font">
-              <tr>
-                <td colspan="2" align="left" valign="top"><div align="justify">Hubungan Kerja LPPN :</div></td>
-                </tr><tr>
-                <td width="5%" align="left" valign="top">1.</td>
-                <td width="95%" valign="top"> <div align="justify"> LPPN dan Pemerintah.</div></td>
-              </tr>
-              <tr>
-                <td align="left" valign="top">2.</td>
-                <td valign="top"><div align="justify"> LPPN dan LPPD(Provinsi/Kab./Kota/Kecamatan).</div></td>
-              </tr>
-              <tr>
-                <td align="left" valign="top">3.</td>
-                <td valign="top"><div align="justify"> LPPN dan Lembaga Keagamaan Kristen.</div></td>
-              </tr>
-              <tr>
-                <td align="left" valign="top">4.</td>
-                <td valign="top"><div align="justify"> LPPN dan Panitia Pelaksana PESPARAWI.</div></td>
-              </tr>
-              <tr>
-                <td align="left" valign="top">5.</td>
-                <td valign="top"><div align="justify"> LPPN dan Sekolah Musik/Sanggar Vokal/ Musik Gerejawi.</div></td>
-              </tr>
-              <tr>
-                <td align="left" valign="top">6.</td>
-                <td valign="top"><div align="justify"> LPPN dan Asosiasi Musisi Kristen.</div></td>
+            Apa Itu Teknik Informatika ?
+Teknik Informatika merupakan kumpulan disiplin ilmu dan teknik yang secara khusus menangani masalah transformasi atau pengolahan data dengan memanfaatkan se-optimal mungkin teknologi komputer melalui proses-proses logika. Pada teknik informatika bidang ilmu yang lebih banyak dikaji adalah bidang pemrograman dan komputasi, rekayasa perangkat lunak (software) untuk berbagai bidang aplikasi dalam berbagai bidang usaha, dan teknologi jaringan komputer.
+                </div></td>
               </tr>
             </table>
           </div></td>
